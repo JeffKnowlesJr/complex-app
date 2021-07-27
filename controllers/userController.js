a3f81b36715844d802623b9ac085f7a68e646d82
JeffKnowlesJr <jeffknowlesjr@gmail.com>
2021-07-27 12:25:04 -0400

    Started Controllers

---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+exports.login = () => {}
+
+exports.logout = () => {}
+
+exports.register = (req, res) => {
+  res.send('thanks for trying to register')
+}
+
+exports.home = (req, res) => {
+  res.render('home-guest')
+}
