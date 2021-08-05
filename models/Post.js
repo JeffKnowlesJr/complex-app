807ece9ba46cc88249caa2166a3c5d4b7c516325
JeffKnowlesJr <jeffknowlesjr@gmail.com>
2021-08-05 13:19:07 -0400

    included visitorId in req and isVisitorOwner post

--- models/Post.js
+++ models/Post.js
@@ -57,7 +57,7 @@ Post.prototype.create = function () {
 }
 
 // This is a mongoose strategy
-Post.reusablePostQuery = function (uniqueOperations) {
+Post.reusablePostQuery = function (uniqueOperations, visitorId) {
   return new Promise(async function (resolve, reject) {
     let aggOpperations = uniqueOperations.concat([
       {
@@ -73,6 +73,7 @@ Post.reusablePostQuery = function (uniqueOperations) {
           title: 1,
           body: 1,
           createdAt: 1,
+          authorId: '$author',
           author: { $arrayElemAt: ['$authorDocument', 0] }
         }
       }
@@ -83,6 +84,8 @@ Post.reusablePostQuery = function (uniqueOperations) {
 
     // clean up author property in each post object
     posts = posts.map((post) => {
+      post.isVisitorOwner = post.authorId.equals(visitorId)
+
       post.author = {
         username: post.author.username,
         avatar: new User(post.author, true).avatar
@@ -94,16 +97,17 @@ Post.reusablePostQuery = function (uniqueOperations) {
   })
 }
 
-Post.findSingleById = function (id) {
+Post.findSingleById = function (id, visitorId) {
   return new Promise(async function (resolve, reject) {
     if (typeof id != 'string' || !ObjectID.isValid(id)) {
       reject()
       return
     }
 
-    let posts = await Post.reusablePostQuery([
-      { $match: { _id: new ObjectID(id) } }
-    ])
+    let posts = await Post.reusablePostQuery(
+      [{ $match: { _id: new ObjectID(id) } }],
+      visitorId
+    )
 
     if (posts.length) {
       console.log(posts[0])
