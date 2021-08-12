0553bac432ff0a153decee73473d9f951de72dbe
JeffKnowlesJr <jeffknowlesjr@gmail.com>
2021-08-11 22:17:52 -0400

    created basic follow feature

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const userController = require('./controllers/userController')
 const postController = require('./controllers/postController')
+const followController = require('./controllers/followController')
 
 // User related routes
 router.get('/', userController.home)
@@ -23,6 +24,13 @@ router.get(
   '/profile/:username',
   userController.ifUserExists,
   userController.profilePostsScreen
+)
+
+// Follow related routes
+router.post(
+  '/addFollow/:username',
+  userController.auth,
+  followController.addFollow
 )
 
 module.exports = router
